0f9981d8c313ea5abdf9f77c9174f035bda53077
Admin <Admin@DESKTOP-C49GG1B>
2022-09-30 18:54:40 +0200

    Добавьте файлы проекта.

---
+++ Богатов Мавтей/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// Богатов Матвей 22-ИСП2/2
+try
+{
+    Console.Write("Введите x:");
+    double x = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите :y");
+    double y = double.Parse(Console.ReadLine());
+    double res = (32 * y) - 32; *5 / 9 = 0 * x;
+        Console.WriteLine($"{res:F2}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
